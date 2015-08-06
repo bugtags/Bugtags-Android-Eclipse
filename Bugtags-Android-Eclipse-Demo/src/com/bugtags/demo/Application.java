c7e367b844f91504ccadecd64f5c4b0363bb3461
Kevin He <he.changbin@bugtags.com>
2015-08-07 00:22:45 +0800

    version 1.0.0

--- Bugtags-Android-Eclipse-Demo/src/com/bugtags/demo/Application.java
+++ Bugtags-Android-Eclipse-Demo/src/com/bugtags/demo/Application.java
@@ -8,6 +8,6 @@ public class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
 
-        Bugtags.start("f9465c17e83c070e9c27934f463b4bde", this, Bugtags.BTGInvocationEventBubble);
+        Bugtags.start("8f443d183b1d202a5b006faa4718e1a6", this, Bugtags.BTGInvocationEventBubble);
     }
 }
